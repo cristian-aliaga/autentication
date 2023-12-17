c7bfeec1dcb99ce785b5df56a1d32fcb20c2c99e
Cristian Aliaga <132721848+cristian-aliaga@users.noreply.github.com>
2023-12-17 11:58:44 -0300

    Primer Commit: Autenticacion, Render, React, MongoDB Atlas

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import { AuthForm } from './components/AuthForm'
+import { UserProvider } from './context/UserContext'
+
+function App() {
+
+  return (
+    <>
+      <UserProvider>
+        <Routes>
+          <Route path='/login' element={<AuthForm />}></Route>
+        </Routes>
+      </UserProvider>
+    </>
+  )
+}
+
+export default App
